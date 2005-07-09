b909ba62295377b4272209843cb5147155a07885
Vivien Mallet <Vivien.Mallet@inria.fr>
2005-07-09 14:08:35 +0000

    Added copyright statements and the GNU GPL file.

--- talos/config_stream.py
+++ talos/config_stream.py
@@ -1,3 +1,27 @@
+# Copyright (C) 2005 CEREA
+#     Authors: Vivien Mallet, Vincent Picavet
+#
+# CEREA (http://www.enpc.fr/cerea/) is a joint laboratory of
+# ENPC (http://www.enpc.fr/) and EDF R&D (http://www.edf.fr/).
+#
+# This file is part of AtmoPy package.
+# AtmoPy is a tool for data processing and visualization in atmospheric
+# sciences.
+#
+# AtmoPy is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# AtmoPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License (file ``license'') for more details.
+#
+# For more information, please see the AtmoPy home page:
+#     http://www.enpc.fr/cerea/atmopy/
+
+
 import commands
 import datetime
     
