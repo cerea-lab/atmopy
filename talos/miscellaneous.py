375e759104381b1e4385d537b4681fe281491f32
Vivien Mallet <Vivien.Mallet@inria.fr>
2005-07-10 15:46:22 +0000

    Initial import.

---
+++ talos/miscellaneous.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2005 CEREA
+#     Authors: Vivien Mallet
+#
+# CEREA (http://www.enpc.fr/cerea/) is a joint laboratory of
+# ENPC (http://www.enpc.fr/) and EDF R&D (http://www.edf.fr/).
+#
+# This file is part of AtmoPy package.
+# AtmoPy is a tool for data processing and visualization in atmospheric
+# sciences.
+#
+# AtmoPy is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# AtmoPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License (file ``license'') for more details.
+#
+# For more information, please see the AtmoPy home page:
+#     http://www.enpc.fr/cerea/atmopy/
+
+
+def is_num(str):
+    """
+    Tests whether a string is a number.
+
+    @type str: string
+    @param str: String to be tested.
+
+    @rtype: Boolean
+    @return: True if 'str' is a number, False otherwise.
+    """
+    is_num = True
+    try:
+        num = float(str)
+    except ValueError:
+        is_num = False
+    return is_num
+
+
+def is_int(str):
+    """
+    Tests whether a string is an integer.
+
+    @type str: string
+    @param str: String to be tested.
+
+    @rtype: Boolean
+    @return: True if 'str' is a integer, False otherwise.
+    """
+    is_num = True
+    try:
+        num = int(str)
+    except ValueError:
+        is_num = False
+    return is_num
+
+
+def remove_file(files):
+    """
+    Removes one or more files and/or directories (without confirmation).
+
+    @type files: string or list of strings
+    @param files: files and directories to be removed.
+    """
+    if isinstance(files, str):
+        files = [files]
+    if not isinstance(files, list):
+        raise ValueError
+    for file in files:
+        import os
+        if os.path.isdir(file):
+            import shutil
+            shutil.rmtree(file)
+        elif os.path.isfile(file):
+            os.remove(file)
