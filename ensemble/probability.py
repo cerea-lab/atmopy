f658c40337875bd73e37d593de8dbfcc9f911596
Vivien Mallet <Vivien.Mallet@inria.fr>
2005-10-19 12:40:13 +0000

    Added the optional argument 'val' to function 'pdf' so that the bins may be defined by the user.

--- ensemble/probability.py
+++ ensemble/probability.py
@@ -26,7 +26,7 @@ from numarray import *
 import scipy.stats.stats
 
 
-def pdf(data, Nbins = 200):
+def pdf(data, Nbins = 200, val = None):
     """
     Computes an approximation of the probability density function. The range
     of data (minimum to maximum) is split into bins of constant length and a
@@ -36,14 +36,29 @@ def pdf(data, Nbins = 200):
     @param data: Data whose probability density function is sought.
     @type Nbins: integer
     @param Nbins: The number of bins.
+    @type val: numarray.array
+    @param val: Values at which the probability density function should be
+    evaluated. Contiguous values are assumed to be at a fixed distance d, and
+    output densities are representative of the interval ]val[i] - d/2, val[i]
+    + d/2]. If 'val' is None, the bins are automatically adapted. If 'val' is
+    not None, 'Nbins' is discarded.
 
-    @rtype: 1D numarray.array
-    @return: The array of probabilities associated with each bin. The mean of
-    this array multiplied by the data range (maximum minus minimum) is equal
-    to one.
+    @rtype: 1D numarray.array, 1D numarray.array
+    @return: The middle values of the bins and the array of probabilities
+    associated with each bin. The mean of this array multiplied by the data
+    range (maximum minus minimum) is equal to one.
     """
-    Nbins = float(Nbins)
     data = sort(ravel(data))
+
+    if val is not None:
+        delta = float(val[1] - val[0])
+        bins = arange(len(val) + 1, typecode = 'f') * delta \
+               + val[0] - 0.5 * delta
+        res = searchsorted(data, bins)
+        return val, (res[1:] - res[:-1]) / (delta * float(len(data)))
+
+    # Bins.
+    Nbins = float(Nbins)
     delta = (data[-1] - data[0]) / Nbins
     bins = arange(Nbins + 1., typecode = 'f') * delta + data[0]
     bins[0] = data[0] - delta   # So that all elements are counted.
